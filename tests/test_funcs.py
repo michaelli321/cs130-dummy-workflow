7eded7addec26a593b30cddf97f332afdc708300
Michael Li <li.michael1998@gmail.com>
2021-01-15 11:50:17 -0800

    Style Eerror

--- tests/test_funcs.py
+++ tests/test_funcs.py
@@ -9,7 +9,7 @@ class TestFuncs(unittest.TestCase):
 	Exercise the functionality given by the dummy funcs
 	'''
 
-	def test_func_1_positive_num(self):
+	def testFunc_1_positive_num(self):
 		res = sheets.func_1(3)
 
 		self.assertEqual(res, 1)
